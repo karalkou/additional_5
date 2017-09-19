2251816ea5e858ff4785f35078a8d083ca65173c
Yury Karalkou <yury.karalkou@gmail.com>
2017-09-19 21:23:20 +0300

    First attempt (without extra cases)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let strArr = str.split(''),
+        tokens = bracketsConfig,
+        stack = [];
+
+    for( let i=0; i<strArr.length; i+=1 ){
+        if( isOpenBracket(strArr[i] ) ){
+            stack.push(strArr[i])
+        }else{
+            if (stack.length === 0
+                ||
+                !isMatches( stack.pop(), strArr[i] )
+            ) {
+                return false;
+            }
+        }
+    }
+    /*-------------------------- helpers --------------------------*/
+    function isOpenBracket(currentItem) {
+        for (let j=0; j<tokens.length; j+=1) {
+            if (tokens[j][0] === currentItem) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function isMatches(topOfStack, closingBracket) {
+        for (let k=0; k<tokens.length; k+=1) {
+            if (
+                tokens[k][0] === topOfStack
+                &&
+                tokens[k][1] === closingBracket
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /*-------------------------- \helpers --------------------------*/
+
+    return stack.length === 0;
+};
